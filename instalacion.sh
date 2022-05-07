325158f827e53b9025ce08c7987fba19ea2d7f86
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-05-07 14:48:26 +0200

    Create instalacion.sh

---
+++ instalacion.sh
@@ -0,0 +1,5 @@
+echo "Bienvenido al asistente de instalacion de MyOS, en este caso no hay nada que instalar, asi que solo es darle permisos de ejecucion al script"
+chmod +x ./myos.sh
+echo "Ya hemos acabado, ahora solo tiene que hacer en su terminal: ./myos.sh"
+echo "Ahora se eliminara el asistente de instalacion de MyOS, Gracias por descargarlo :) ahora a disfrutar"
+rm instalacion.sh
